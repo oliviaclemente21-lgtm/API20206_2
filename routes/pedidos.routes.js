9f85f1d252a37061bfe1e19c786d2b96fcd65b9d
oliviaclemente21-lgtm <oliviaclemente21@gmail.com>
2026-06-30 14:43:10 -0500

    Update pedidos.routes.js

--- routes/pedidos.routes.js
+++ routes/pedidos.routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { postPedido, getPedidos, getPedidoDetalle } from '../controladores/pedidosCtrl.js';
+// IMPORTANTE: Aquí cambiamos 'postPedido' por 'guardarPedido'
+import { guardarPedido, getPedidos, getPedidoDetalle } from '../controladores/pedidosCtrl.js';
 
 const router = Router();
 
 router.get('/pedidos', getPedidos);
 router.get('/pedidos/:id/detalle', getPedidoDetalle);
-router.post('/pedidos', postPedido);
+
+// Usamos la función 'guardarPedido' que acabamos de definir en el controlador
+router.post('/pedidos', guardarPedido);
 
 export default router;
